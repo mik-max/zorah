8ee015306071aaeedd8d6a9e5a245949883a0c42
Michael chinye <michaelchinye2018@gmail.com>
2025-01-19 23:14:35 +0100

    fix: added text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
                <div className="relative z-2 max-w-512 max-xl:max-w-388">
                     <div className='caption small-2 uppercase text-p3'>Video Editing</div>
                     <h1 className='mb-6 h1 p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12'>Amazingly simple</h1>
-                    <p className='max-w-440 mb-14 body-1 max-md:mb-10'>We designed XORA AI video Editor to be an easy to use, quick to learn and suprisingly powerful tool</p>
+                    <p className='max-w-440 mb-14 body-1 max-md:mb-10'>We designed XORA AI video Editor to be an easy to use, quick to learn and suprisingly powerful tool for all. </p>
                </div>
           </div>
      </Element>
